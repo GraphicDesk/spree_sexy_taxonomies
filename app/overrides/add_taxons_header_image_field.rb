85de9302f3e2f9640fddc9698bc8c301d5d4df34
GraphicDesk <office@graphicdesk.ro>
2015-12-30 00:10:21 +0200

    Updates the insert point of the Header image loading for the admin section

--- app/overrides/add_taxons_header_image_field.rb
+++ app/overrides/add_taxons_header_image_field.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/taxons/_form",
                      :name => "add_taxon_header_properties",
-                     :insert_bottom => ".alpha.five.columns",
+                     :insert_after => '.help-block',
                      :partial => "spree/admin/taxons/header_image_fields",
                      :disabled => false)
